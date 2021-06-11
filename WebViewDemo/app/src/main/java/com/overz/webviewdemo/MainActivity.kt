f7b26076e777bde01e383f151edd585b91c82c66
zhuyongchao <over_z@outlook.com>
2021-06-11 22:03:01 +0800

    WebView 实现安卓显示网页 公司要求可以实现给网页传参，并显示数据 1、网页为本地网页显示 2、传参以后改变网页显示内容 3、具体查看WebView怎么用，操作点击事件怎么使用

---
+++ WebViewDemo/app/src/main/java/com/overz/webviewdemo/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.overz.webviewdemo
+
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webview)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        webView.loadUrl("file:android_asset/Typora.html")
+    }
+
+}
